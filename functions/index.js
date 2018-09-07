99c4207d95540fa9a7dd28e32999b1f6b26dfc89
Marco Acierno <marcoaciernoemail@gmail.com>
2018-09-07 19:02:44 +0200

    Add eventbrite webhook

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,8 @@
 const functions = require('firebase-functions');
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.eventbriteWebhook = functions.https.onRequest((request, response) => {
+    console.log('--------------------------------------------');
+    console.log('new logs');
+    console.log('body is:', request.body);
     response.send('Hello from Firebase!');
 });
