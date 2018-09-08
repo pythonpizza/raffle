f0035ea73bb0eaf4a6dc81032f59521423c79edd
Dario Carella <dario.kar@gmail.com>
2018-09-08 13:32:48 +0200

    Improve temp entries array and pizzas preoad

--- frontend/src/Components/App/index.tsx
+++ frontend/src/Components/App/index.tsx
@@ -18,34 +18,7 @@ export default class App extends React.Component {
     componentWillMount() {
         const loadEntriesPromise = new Promise(resolve => setTimeout(resolve, 1000 * Math.random())).then(() =>
             this.setState({
-                entries: [
-                    1,
-                    3,
-                    4,
-                    7,
-                    11,
-                    12,
-                    18,
-                    24,
-                    29,
-                    32,
-                    38,
-                    47,
-                    48,
-                    54,
-                    57,
-                    64,
-                    69,
-                    71,
-                    76,
-                    83,
-                    86,
-                    89,
-                    91,
-                    99,
-                    111,
-                    123,
-                ],
+                entries: Array.from(Array(64).keys()).map((_, i) => i + 1),
             })
         );
         const preloadPizza = (i: number) => {
@@ -56,9 +29,7 @@ export default class App extends React.Component {
                 image.src = require(`Assets/images/pizza--${i}.png`);
             });
         };
-        const preloadPizzasPromise = Promise.all(
-            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(i => preloadPizza(i))
-        );
+        const preloadPizzasPromise = Promise.all(Array.from(Array(17).keys()).map(i => preloadPizza(i)));
 
         Promise.all([loadEntriesPromise, preloadPizzasPromise]).then(() => this.setState({ isLoading: false }));
     }
